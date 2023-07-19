f29626130e04b700b6533e30eb6c22553fb4fa94
Jing Zhao <122287325+JZhao777@users.noreply.github.com>
2023-07-19 16:17:25 -0400

    first update

---
+++ ClassCraftFinal/Pages/ProductList.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassCraftFinal.Models;
+using ClassCraftFinal.Services;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClassCraftFinal.Pages
+{
+    public class ProductListModel : PageModel
+    {
+        public JsonFileProductService ProductService;
+
+        public IEnumerable<Product> Products { get; private set; } = default!;
+
+       public ProductListModel(JsonFileProductService productService)
+        {
+            
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
---
+++ ClassCraftFinal/Services/JsonFileProductService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using ClassCraftFinal.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ClassCraftFinal.Services
+{
+	public class JsonFileProductService
+	{
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+        //this <product> refers to the Product.cs under Models
+        public IEnumerable<Product> GetProducts()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                })!;
+        }
+        }
+}
+
